beb8501060bdc899a82243278a8e5ea60b6bb1aa
林家宏 Joe <jaihongmini@gmail.com>
2020-09-21 12:35:55 +0800

    [預先建置] 先替換型別

--- projects/demo-form/schematics/ng-generate/hello/index.ts
+++ projects/demo-form/schematics/ng-generate/hello/index.ts
@@ -1,7 +1,8 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
+import { Schema } from './schema';
 
 // Just return the tree
-export function hello(options: any): Rule {
+export function hello(options: Schema): Rule {
   return (tree: Tree) => {
     return tree;
   };
