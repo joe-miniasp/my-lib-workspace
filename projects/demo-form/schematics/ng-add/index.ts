ab4b5787f503cf915fab90b96938422819cb2efa
林家宏 Joe <jaihongmini@gmail.com>
2020-09-22 16:43:31 +0800

    先把import的部分寫好

--- projects/demo-form/schematics/ng-add/index.ts
+++ projects/demo-form/schematics/ng-add/index.ts
@@ -1,10 +1,24 @@
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+import {
+  addModuleImportToRootModule,
+  getProjectFromWorkspace,
+} from '@angular/cdk/schematics';
+
+import {
+  NodeDependencyType,
+  addPackageJsonDependency,
+} from '@schematics/angular/utility/dependencies';
+
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { WorkspaceProject } from '@schematics/angular/utility/workspace-models';
 
 import { Schema } from './schema';
 
 // Just return the tree
 export default function ngAdd(options: Schema): Rule {
-  return (tree: Tree) => {
+  return (tree: Tree, context: SchematicContext) => {
     return tree;
   };
 }
